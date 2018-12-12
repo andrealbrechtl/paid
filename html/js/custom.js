5800af011e9d6a6b70ee6919cefc03bebc82d5af
Andre Albrecht <andre.albrecht@matmatch.com>
2018-12-12 14:18:50 +0100

    new js

--- html/js/custom.js
+++ html/js/custom.js
@@ -415,8 +415,8 @@ document.getElementById('btn_sup_3_p').addEventListener('click', function(e) {
 })
 
 //hotjar
-<!-- Hotjar Tracking Code for https://matmatch.com/ -->
-<script>
+
+
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:724187,hjsv:6};
@@ -425,7 +425,7 @@ document.getElementById('btn_sup_3_p').addEventListener('click', function(e) {
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+
 
 //intercom
 
