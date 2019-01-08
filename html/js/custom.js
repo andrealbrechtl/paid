d9e172de2a7324e65ae5ef721a06137bc468f307
Andre Albrecht <andre.albrecht@matmatch.com>
2019-01-08 09:25:57 +0100

    fix css issues and material id url

--- html/js/custom.js
+++ html/js/custom.js
@@ -20,6 +20,23 @@
  9. INPUT FIELD EFFECT
 
  */
+
+ //get parans
+ var getUrlParameter = function getUrlParameter(sParam) {
+ var sPageURL = window.location.search.substring(1),
+     sURLVariables = sPageURL.split('&'),
+     sParameterName,
+     i;
+
+ for (i = 0; i < sURLVariables.length; i++) {
+     sParameterName = sURLVariables[i].split('=');
+
+     if (sParameterName[0] === sParam) {
+         return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+     }
+ }
+};
+
 (function () {
   'use strict';
 
@@ -303,6 +320,14 @@
 //sendform
 
 function sendform(form) {
+
+if (getUrlParameter("matid")){
+
+  form["materialid"] = getUrlParameter("matid")
+
+  }
+  console.log(form)
+
   var request = $.ajax({
                     url: "https://workersapp.herokuapp.com/request-form",
                     type: "POST",
